6f843ec796a9a04002a740a903672e56e971e035
johnpathadan <johnpathadan@gmail.com>
2026-02-23 13:15:11 +0530

    Initialized the project

---
+++ src/Raffle.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.19;
+
+//Natspec below
+/**
+    * @title A Sample Raffle contract
+    * @author John Paul
+    * @notice   This contract is for creating a sample raffle
+    * @dev Implements Chainlink VRFv2.5
+ */
+contract Raffle{}
