2cb42262ae58f9dc20601a5c047db8b129d3b52c
johnpathadan <johnpathadan@gmail.com>
2026-02-23 12:55:19 +0000

    feat: event/emit added

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,11 +1,37 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
-//Natspec below
 /**
     * @title A Sample Raffle contract
     * @author John Paul
     * @notice   This contract is for creating a sample raffle
     * @dev Implements Chainlink VRFv2.5
  */
-contract Raffle{}
+contract Raffle{
+    error Raffle__SendMoreToEnterRaffle();
+
+    uint256 private immutable i_entranceFee;
+    address[] private s_players;
+
+    event RaffleEntered(address indexed player);
+
+    constructor(uint256 entranceFee){
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable{
+        if(msg.value < i_entranceFee){
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+        
+        s_players.push(address(msg.sender));
+        emit RaffleEntered(msg.sender);
+    }
+
+    function pickWinner() public{}
+
+    //Getter Functions below
+    function getEntranceFee() external view returns(uint256) {
+        return i_entranceFee;
+    }
+}
